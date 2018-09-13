0392915b7782d028f33b901d3aca6db24e4992d8
eduardo <cs.luizeduardo@hotmail.com>
2018-09-13 12:51:23 -0300

    JS and CSS Packages Added

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/dashboard', 'Web\DashboardController');
+
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
