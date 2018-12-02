b4896dcc2c732f3d4df042208fc0de1f8de78c57
luizsoares <2304>
2018-12-02 15:02:06 -0300

    Removing commit when debugs.

--- app/Console/Commands/invalidateDueTickets.php
+++ app/Console/Commands/invalidateDueTickets.php
@@ -52,10 +52,10 @@ class invalidateDueTickets extends Command
 
         $this->warn($dueOpeneds . " Tickets with due date");
 
-//        $tickets->update([
-//                "status_id" => Status::where('name', __('messages.ticket_status_expired'))->first()->id,
-//                "rating" => 0
-//        ]);
+        $tickets->update([
+                "status_id" => Status::where('name', __('messages.ticket_status_expired'))->first()->id,
+                "rating" => 0
+        ]);
 
         $this->error("Expiring " . $dueOpeneds . " tickets");
 
