2cae8008becebdd21c98f8662f9a6af56cc503d6
eduardo <cs.luizeduardo@hotmail.com>
2018-12-03 11:14:22 -0200

    Added logo and app name to env

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/dashboard') }}">
-                    Interdesk
+                    <img src="{{ env('APP_LOGO') }}" width="30"> {{ env('APP_NAME') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
