01f3a0c44c771148ab4ed6f86878db5cdbfcbdf9
luizsoares <2304>
2019-02-28 10:03:21 -0300

    Removed users with deleted at from ticket display

--- app/Models/Observer.php
+++ app/Models/Observer.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Observer extends Model
 {
     public function user() {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->whereNull('deleted_at');
     }
 }
